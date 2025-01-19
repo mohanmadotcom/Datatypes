76e050a49f8c113f963bcca186a23ee966028551
mohanmadotcom <mohanmadotcom@gmail.com>
2025-01-19 19:37:13 +0530

    first commit

---
+++ datatypes.py
@@ -0,0 +1,18 @@
+#Subscripting
+print("Hello"[2])
+
+#String
+print("123"+"345")
+
+#Integer
+print(123 + 345)
+
+#Large Integers
+print(123_456_789)
+
+#Float
+print(3.14)
+
+#Boolean
+print(True)
+print(False)
